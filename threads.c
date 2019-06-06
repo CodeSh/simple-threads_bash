a4076b5f0093e3c020d702779b9212daa87e07e9
Harry <otgo@outlook.es>
2019-06-06 02:38:50 -0500

    rm unused var

--- threads.c
+++ threads.c
@@ -2,15 +2,10 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
-
-// Let us create a global variable to change it in threads
-int g = 0;
-
 // The function to be executed by all threads
 void *commands(char *command) {
 	system(command);
 }
-
 int main(int argc, char *argv[]) {
 	int i;
 	pthread_t tid;
